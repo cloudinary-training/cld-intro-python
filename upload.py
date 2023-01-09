334708359f2fd1b6599385b548945c871c791e6c
jenbrissman <brissman514@gmail.com>
2023-01-09 15:06:57 -0500

    changing order of remote source

--- upload.py
+++ upload.py
@@ -12,8 +12,31 @@ print(config.cloud_name)
 print(config.api_key)
 
 # Basic Upload Method
-basic_upload = uploader.upload("./assets/llama.jpg")
-print("**** Basic upload ****\nDelivery URL: ", json.dumps(basic_upload,indent=2), "\n")
+# basic_upload = uploader.upload("./assets/llama.jpg")
+# print("**** Basic upload ****\nDelivery URL: ", json.dumps(basic_upload,indent=2), "\n")
+
+ 
+# # Upload Source Options 
+
+# #HTTP or HTTPS URL - Wikimedia
+# https = uploader.upload("https://upload.wikimedia.org/wikipedia/commons/9/99/Capivara%28Hydrochoerus_hydrochaeris%29.jpg")
+# print("**** Wikimedia ****\nDelivery URL: ", json.dumps(https,indent=2), "\n")
+
+# # #Base64 Data URI
+# base64 = uploader.upload("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=")
+# print("**** Base 64 Data URI ****\nDelivery URL: ", json.dumps(base64,indent=2), "\n")
+
+# #Private Storage URL - Amazon S3 or Google Cloud Storage - we have not set up an S3 bucket to demo this,
+# but wanted to show the format to show it's possible to use these remote sources
+# s3 = uploader.upload("s3://my-bucket/my-path/example.jpg")
+# print("**** Amazon S3 ****\nDelivery URL: ", json.dumps(s3,indent=2), "\n")
+
+# #FTP URL - we have not set up an FTP server to demo this,
+# but wanted to show the format to show it's possible to use these remote sources
+# ftp = uploader.upload("ftp:user1:mypass@ftp.example.com/sample.jpg")
+# print("**** FTP URL ****\nDelivery URL: ", json.dumps(ftp,indent=2), "\n")
+
+
  
 # # Resource Type
 # resource_vid = uploader.upload("assets/glacier.mp4", resource_type = "video")
@@ -66,22 +89,3 @@ print("**** Basic upload ****\nDelivery URL: ", json.dumps(basic_upload,indent=2
 # print("**** Create Folder called argentina, with a subfolder called animals inside it, and upload image to that folder without a specified public ID ****\nDelivery URL: ", json.dumps(folder_within_folder,indent=2), "\n")
 
 
-# # Upload Source Options 
-
-# #HTTP or HTTPS URL - Wikimedia
-# https = uploader.upload("https://upload.wikimedia.org/wikipedia/commons/9/99/Capivara%28Hydrochoerus_hydrochaeris%29.jpg")
-# print("**** Wikimedia ****\nDelivery URL: ", json.dumps(https,indent=2), "\n")
-
-# # #Base64 Data URI
-# base64 = uploader.upload("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=")
-# print("**** Base 64 Data URI ****\nDelivery URL: ", json.dumps(base64,indent=2), "\n")
-
-# #Private Storage URL - Amazon S3 or Google Cloud Storage - we have not set up an S3 bucket to demo this,
-# but wanted to show the format to show it's possible to use these remote sources
-# s3 = uploader.upload("s3://my-bucket/my-path/example.jpg")
-# print("**** Amazon S3 ****\nDelivery URL: ", json.dumps(s3,indent=2), "\n")
-
-# #FTP URL - we have not set up an FTP server to demo this,
-# but wanted to show the format to show it's possible to use these remote sources
-# ftp = uploader.upload("ftp:user1:mypass@ftp.example.com/sample.jpg")
-# print("**** FTP URL ****\nDelivery URL: ", json.dumps(ftp,indent=2), "\n")
