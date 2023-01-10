59102f8098a56ab23107c559fae3b1b1b1c18f24
jenbrissman <brissman514@gmail.com>
2023-01-10 13:47:40 -0500

    deleting builder.py

--- utilities.py
+++ utilities.py
@@ -1,13 +1,7 @@
-import json
-from logging import NOTSET, lastResort
-
 from dotenv import load_dotenv
 load_dotenv()
 
 import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-from cloudinary.api import delete_resources_by_tag, resources_by_tag
 from cloudinary.utils import cloudinary_url
 
 # get reference to config instance, return "https" URLs by setting secure=True
@@ -33,7 +27,6 @@ url, options = cloudinary_url(
 )
 print("**** Return only the URL --> " + url, "\n")
 
-
 # To create an image tag
 print(cloudinary.CloudinaryImage("puma").image())
 
@@ -45,30 +38,3 @@ print(cloudinary.CloudinaryImage("puma").build_url())
 
 # Build URL using build_url method (video)
 print(cloudinary.CloudinaryVideo("tortoise").build_url())
-
-# run upload
-# result = cloudinary.uploader.upload('https://res.cloudinary.com/jen-brissman/image/upload/puma')
-# print upload response and secure URL
-# print(json.dumps(result,indent=2))
-# print (result['secure_url'])
-
-# import utils to help with making transformations
-# from cloudinary.utils import cloudinary_url
-# create transformation url using public from git
-# url, options = cloudinary_url(
-#         result['public_id'],
-#         width=500,
-#         crop="scale"
-#     )
-# print(url)
-
-# To return only the URL, standard Python command - cloudinary.utils.cloudinary_url
-# print(cloudinary.utils.cloudinary_url(puma ,resource_type="image"))
-# print(cloudinary.utils.cloudinary_url(
-#     "tortoise", resource_type="video"))
-
-
-# # direct url building
-# cloudinary.CloudinaryImage("sample.jpg").build_url(
-#   width = 100, height = 150, crop = 'fill')
-
