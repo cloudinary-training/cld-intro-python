65d3a7cc10c60ed6ffcd354b020f50bf0c875fcd
jenbrissman <brissman514@gmail.com>
2023-01-11 16:26:09 -0500

    adding mistake to the backup example

--- backup.py
+++ backup.py
@@ -20,3 +20,10 @@ backup = uploader.upload("assets/sealion.jpg",
 
 print("**** Upload and ensure asset is backed up ****\n Response:\n", json.dumps(backup,indent=2), "\n")
 
+# Example to show a restore when a mistake is made
+mistake = uploader.upload("assets/puma.jpg", 
+  public_id="sealion0",
+  overwrite="True",
+  backup = True)
+
+print("**** Upload another asset by mistake with the same public ID ****\n Response:\n", json.dumps(mistake,indent=2), "\n")
--- presets.py
+++ presets.py
@@ -16,18 +16,18 @@ print(config.api_key)
 
 
 # Create an unsigned preset (allows unsigned uploading), adds the tag 'remote' to uploaded images, and only allows the uploading of JPG and PNG image formats:
-create_unsigned = api.create_upload_preset(
-  name = "unsigned-image",
-  unsigned = True, 
-  tags = "remote", 
-  allowed_formats = "jpg,png")
-print(json.dumps(create_unsigned, indent=2))
-
-# Create a signed preset (does not allow unsigned uploading), adds the tag 'remote' to uploaded images, and only allows the uploading of JPG and PNG image formats:
-# create_signed = api.create_upload_preset(
-#   name = "signed-image",
-#   unsigned = False, #default
+# create_unsigned = api.create_upload_preset(
+#   name = "unsigned-image",
+#   unsigned = True, 
 #   tags = "remote", 
 #   allowed_formats = "jpg,png")
-# print(json.dumps(create_signed, indent=2))
+# print(json.dumps(create_unsigned, indent=2))
+
+# Create a signed preset (does not allow unsigned uploading), adds the tag 'remote' to uploaded images, and only allows the uploading of JPG and PNG image formats:
+create_signed = api.create_upload_preset(
+  name = "signed-image",
+  unsigned = False, #default
+  tags = "remote", 
+  allowed_formats = "jpg,png")
+print(json.dumps(create_signed, indent=2))
 
